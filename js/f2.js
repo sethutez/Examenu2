b832898c87ea19adb10c9bc2a0bd08f13e2680ab
sethutez <84397152+sethutez@users.noreply.github.com>
2021-06-30 10:58:28 -0500

    Examen unidad 2

--- js/f2.js
+++
@@ -1,6 +0,0 @@
-function iva(cantidad){
-	var iv= parseInt(cantidad);
-	var caliva=parseInt(iv * .16);
-	var total= caliva;
-    document.getElementById("iva").innerHTML="<h1>Total del IVA es:" + total + "</h1>";
-}
